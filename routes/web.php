aa6168d0deda982b318afb9aadb5092fd891b5c1
fremasadi <fremasadii@gmail.com>
2026-04-06 17:02:01 +0700

    feat

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,15 @@ Route::get('/', function () {
 
 Route::get('/symlink', function () {
     try {
-        \Illuminate\Support\Facades\Artisan::call('storage:link');
-        return 'Storage link berhasil dibuat!';
+        $targetFolder = storage_path('app/public');
+        $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage'; // Bypassing public_path() just in case
+        
+        if (!file_exists($linkFolder)) {
+            symlink($targetFolder, $linkFolder);
+            return 'Storage link berhasil dibuat manual by PHP!';
+        }
+        
+        return 'Storage link sudah ada.';
     } catch (\Exception $e) {
         return 'Error: ' . $e->getMessage();
     }
