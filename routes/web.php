786ed66d4a5b30012605c3bd02bb8ac2a79d54fd
fremasadi <fremasadii@gmail.com>
2026-04-06 16:59:30 +0700

    feat:all from mac

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('front.pages.home');
 })->name('home');
+
+Route::get('/symlink', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('storage:link');
+        return 'Storage link berhasil dibuat!';
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
+});
